dc6fce42574417a8af4b8137265556b955aaebd2
Sondre Sætre Edvardsen <sondre@ridgeclaw.mshome.net>
2026-02-25 11:14:13 +0100

    Apply refactor: move login and main_area into lib/pages (from workspace)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+import 'pages/main_area.dart';
 
 void main() => runApp(PosApp());
 
@@ -12,60 +14,6 @@ class PosApp extends StatelessWidget {
         '/': (context) => LoginPage(),
         '/main': (context) => MainArea(),
       },
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  final TextEditingController _userController = TextEditingController();
-  final TextEditingController _passController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: 360,
-          padding: EdgeInsets.all(24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Sign in', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-              SizedBox(height: 16),
-              TextField(
-                controller: _userController,
-                decoration: InputDecoration(labelText: 'Username', border: OutlineInputBorder()),
-              ),
-              SizedBox(height: 12),
-              TextField(
-                controller: _passController,
-                decoration: InputDecoration(labelText: 'Password', border: OutlineInputBorder()),
-                obscureText: true,
-              ),
-              SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushReplacementNamed('/main');
-                  },
-                  child: Text('Login'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MainArea extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('POS26 Main')),
-      body: Center(child: Text('Main area - protected')),
     );
   }
 }
---
+++ lib/pages/main_area.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MainArea extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('POS26 Main')),
+      body: Center(child: Text('Main area - protected')),
+    );
+  }
+}
